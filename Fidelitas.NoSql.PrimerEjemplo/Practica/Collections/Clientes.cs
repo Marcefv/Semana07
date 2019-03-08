a187a0a9d0f0b247bdf283425fc35c9b8276ba6e
Laboratorios <marcefv.89@gmail.com>
2019-03-07 19:13:29 -0600

    clases de propiedades

---
+++ Fidelitas.NoSql.PrimerEjemplo/Practica/Collections/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica.Collections
+{
+    public class Clientes
+    {
+        public int id { get; set; }
+
+        public String nombre { get; set; }
+
+        public int telefono { get; set; }
+
+        public Renta renta { get; set; }
+    }
+}
---
+++ Fidelitas.NoSql.PrimerEjemplo/Practica/Collections/Renta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica.Collections
+{
+    public class Renta
+    {
+        public DateTime fecha_inicio { get; set; }
+        public DateTime fecha_entrega { get; set; }
+
+        public DateTime entrega_real { get; set; }
+
+        public int dias_atraso { get; set; }
+
+       
+    }
+}
